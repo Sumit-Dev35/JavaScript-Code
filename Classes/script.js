4128a4672a4a8722ae0a8165bb7dfeb3a153723e
Sumit Tiwari <sumitt.dev35@gmail.com>
2025-06-14 23:10:15 +0000

    Implement class implementation in javascript

---
+++ Classes/script.js
@@ -0,0 +1,10 @@
+class Rectangle{
+   constructor(length,bredth){
+     this.length=length;
+     this.bredth=bredth;
+   }
+}
+let obj1=new Rectangle(10,10);
+console.log(obj1);
+console.table(obj1);
+
