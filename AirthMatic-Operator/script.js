687555db96aa276b439a1444ee25a8ab9735d3c7
Sumit-Dev35 <sumitt.dev35@gmail.com>
2025-06-20 21:37:21 +0530

    Read about thr airthmatic operator in javascript

---
+++ AirthMatic-Operator/script.js
@@ -0,0 +1,11 @@
+const now = 2025;
+const vinayAge = now - 2003;
+const sumitAge = now - 2001;
+console.log(vinayAge, sumitAge);
+console.log(2 * vinayAge, sumitAge / 10, 2 ** 3);
+const firstName = "Vinay";
+const lastName = "Tiwari";
+console.log(firstName + " " + lastName);
+
+let x = 10 + 5;
+console.log(x);
