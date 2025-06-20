600006ab3331392d3dff5c6a432a1fc653f0e574
Sumit-Dev35 <sumitt.dev35@gmail.com>
2025-06-20 21:23:09 +0530

    read about var let and const keywords and its usage

---
+++ var-let-const/script.js
@@ -0,0 +1,10 @@
+console.log(variable1);
+var variable1 = "Sumit Tiwari";
+console.log(variable1);
+
+console.log(variable1);
+let variable1 = "Sumit tiwari";
+console.log(variable1);
+
+const variable1 = "Sumit";
+console.log(variable1);
