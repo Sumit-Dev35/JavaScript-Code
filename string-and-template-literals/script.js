775e2e52e7025430ad67b639618b67d1a79f8e41
Sumit-Dev35 <sumitt.dev35@gmail.com>
2025-06-21 21:01:02 +0530

    Read about the template string in javascript

---
+++ string-and-template-literals/script.js
@@ -0,0 +1,7 @@
+const firstName = "Sumit";
+const job = "Developer";
+const birthYear = 2001;
+const year = 2025;
+//Template String In JavaScript
+const messgae = `I'm ${firstName}, a ${year - birthYear} years old ${job} !`;
+console.log(messgae);
