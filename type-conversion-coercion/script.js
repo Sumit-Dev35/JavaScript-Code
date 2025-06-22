45f05c71ed3351002a05db297783de37037f1d2a
Sumit-Dev35 <sumitt.dev35@gmail.com>
2025-06-23 00:13:34 +0530

    Read about type converion using String and Number function and also coresion of data type and guess some output

---
+++ type-conversion-coercion/script.js
@@ -0,0 +1,22 @@
+//Type Conversion
+const inputYear = "1991";
+console.log(Number(inputYear));
+console.log(inputYear + 18);
+console.log(Number(inputYear) + 19);
+console.log(Number("1902sumit"));
+console.log(typeof NaN);
+
+console.log(String(28), typeof String(28));
+
+//Type Coersion
+
+console.log("I am " + 23 + " years old");
+console.log("23" + "10" + 3);
+console.log("23" * 2);
+console.log("23" / 2);
+
+//Guess Output
+
+let n = "1" + 1;
+n -= 1;
+console.log(n);
