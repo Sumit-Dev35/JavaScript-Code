0a35e9960b660ed9e47168dc6a96bd3ccb15bc9e
Sumit-Dev35 <sumitt.dev35@gmail.com>
2025-06-23 18:44:30 +0530

    Read about strict mode concept in JavaScript

---
+++ strict-mode/script.js
@@ -0,0 +1,15 @@
+"use strict";
+let hasDriversLicence = false;
+const passTest = true;
+
+if (passTest) {
+  hasDriverLicence = true;
+}
+
+if (hasDriversLicence) {
+  console.log("I can drive");
+}
+
+const interface = "Audio";
+const private = 534;
+// const if=23;
