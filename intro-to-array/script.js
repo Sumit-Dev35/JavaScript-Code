d493b76ebf9bff9c8cbf30665dc96df68744b2a7
Sumit-Dev35 <sumitt.dev35@gmail.com>
2025-06-30 00:41:16 +0530

    Learn about the array in js

---
+++ intro-to-array/script.js
@@ -0,0 +1,18 @@
+const myFirstArray = ["Sumit", "Vinay", "Vikash", "Vishal", "Anurag", "Ayush"];
+console.log(myFirstArray);
+
+const years = new Array(1991, 1984, 2008, 2020);
+console.log(years);
+
+console.log(myFirstArray[0]);
+console.log(myFirstArray[1]);
+console.log(myFirstArray[2]);
+
+console.log(myFirstArray.length, years.length);
+
+//to update the value in the array
+myFirstArray[0] = "dcbvchd";
+console.log(myFirstArray);
+
+const info = ["Suumit", "Tiwari", "System Engineer", 2024];
+console.log(info);
