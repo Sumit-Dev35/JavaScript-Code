b38e8b00c8e33efc2b722af162551d3e4cf44798
Sumit-Dev35 <sumitt.dev35@gmail.com>
2025-06-30 00:52:48 +0530

    Learn about basic function in array

---
+++ array-operation/script.js
@@ -0,0 +1,19 @@
+// const friends=[1,2,3,4,5];
+// console.log(friends);
+// friends.push(6);
+// console.log(friends);
+
+const arr=[1,2,3,4,5];
+console.log(arr.push(6));
+console.log(arr);
+arr.unshift(0);
+console.log(arr);
+console.log(arr.pop());
+console.log(arr);
+console.log(arr.shift());
+console.log(arr);
+console.log(arr.indexOf(3));
+console.log(arr.indexOf(10));
+console.log(arr.includes(3));
+console.log(arr.includes(10));
+
