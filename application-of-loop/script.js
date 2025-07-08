e5f6d6261f97fcf33aa367d658cc099071c4d0fa
Sumit-Dev35 <sumitt.dev35@gmail.com>
2025-07-08 23:54:31 +0530

    explore some application of for loop

---
+++ application-of-loop/script.js
@@ -0,0 +1,8 @@
+let arr=[];
+for(let i=0;i<10;i++){
+  arr.push(i+1);
+}
+
+for(let i=0;i<10;i++){
+  console.log(`Value at ${i} is ${arr[i]}`);
+}
---
+++ intro-to-object/script.js
@@ -0,0 +1,17 @@
+const obj={
+  firstName:"Sumit",
+  lastName:"Tiwari",
+  age:25,
+  designation:"Software Engineer"
+}
+
+console.log(obj);
+console.log(obj.firstName);
+console.log(obj.lastName);
+console.log(obj.age);
+console.log(obj.designation);
+
+console.log(obj["firstName"]);
+console.log(obj["lastName"]);
+console.log(obj["age"]);
+console.log(obj["designation"]);
